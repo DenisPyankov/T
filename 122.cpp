22e478a54ec4d9f439e73aa23e343ea1f017f8f1
Денис Пьянков <91528031+DenisPyankov@users.noreply.github.com>
2022-04-23 09:40:11 +0500

    Add files via upload

---
+++ 122.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    ifstream in("in.txt");
+    ofstream out("out.txt");
+
+    int N;
+    in >> N;
+    vector <int> v;
+    for (int i = 0; i < N; i++) {
+        int a;
+        in >> a;
+        v.push_back(a);
+    }
+    int max = 0, k = 1;
+    
+    for (int i = 0; i < N - 1; i++) {
+        if (v[i] <= v[i + 1])
+            k++;
+        else {
+            if (k > max)
+                max = k;
+            k = 1;
+ 
+        }
+    }
+
+
+    out << max;
+
+
+}
